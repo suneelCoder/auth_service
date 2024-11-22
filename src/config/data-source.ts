62597a1eccbe43260eaa5207226acfc8ee2ca937
suneel bhai <suneel@ceoitbox.in>
2024-11-23 02:31:09 +0530

    final

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -10,7 +10,7 @@ export const AppDataSource = new DataSource({
     password: Config.DB_PASSWORD,
     database: Config.DB_NAME,
     // Don't use this in production. Always keep false
-    synchronize: false,
+    synchronize: true,
     logging: false,
     entities: ["src/entity/*.{ts,js}"],
     migrations: ["src/migration/*.{ts,js}"],
